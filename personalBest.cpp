296033469c724400cb14fa43280feb7867c522e5
Jerry Michael Sypkens Jr <jmsypkens@usfca.edu>
2016-03-14 07:48:14 +0000

    Personal Best

---
+++ personalBest.cpp
@@ -0,0 +1,6 @@
+// 13. Personal Best
+// Write a program that asks for the name of a pole vaulter and the dates and vault heights (in
+// meters) of the athlete’s three best vaults. It should then report in height order (best ﬁrst),
+// the date on which each vault was made, and its height.
+// Input Validation: Only allow the program to accept values between 2.0 and 5.0 for
+// the heights.

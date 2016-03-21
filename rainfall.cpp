0f6275e870fd6c20be5395be7ff2b5c4c5c9735b
Jerry Michael Sypkens Jr <jmsypkens@usfca.edu>
2016-03-21 05:56:03 +0000

    new assignments

---
+++ rainfall.cpp
@@ -0,0 +1,15 @@
+// Jerry M. Sypkens 
+// COS 393 8100 Prof. P. Ghaforyfard
+// Week 2 Homework Due March 7, 2016 - 8. Box Office
+
+/* 24. Using Files—Total and Average Rainfall
+Write a program that reads in from a ﬁle a starting month name, an ending month name,
+and then the monthly rainfall for each month during that period. As it does this, it should
+sum the rainfall amounts and then report the total rainfall and average rainfall for the
+period. For example, the output might look like this:  
+During the months of March–June the total rainfall was 7.32 inches and the average
+monthly rainfall was 1.83 inches.
+Data for the program can be found in the Rainfall.txt ﬁle.
+Hint: After reading in the month names, you will need to read in rain amounts until
+the EOF is reached, and count how many pieces of rain data you read in.
+*/ 

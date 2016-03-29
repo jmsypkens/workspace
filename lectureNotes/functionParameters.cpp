8af01facf623a63cd2b738354c191e97d4f92469
Jerry Michael Sypkens Jr <jmsypkens@usfca.edu>
2016-03-29 21:53:23 +0000

    function parameters exercise

---
+++ lectureNotes/functionParameters.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// function prototype 
+void displayValue(int num);
+
+
+int main()
+{
+    cout << "I am passing 5 to displayValue\n"; 
+    displayValue(5);    // call displayValue w/ argument 5
+    cout << "Back to main. \n";
+    
+    return 0; 
+    
+}
+
+/********************************
+ *          displayValue()
+ * This function uses an interger 
+ * parameter whose value is 
+ * displayed.
+ ********************************/
+ 
+ void displayValue(int num)
+ {
+     cout << "The value is " << num << endl; 
+ }
